8eac98d3e59fdc250ddeef907730000c8a088b4b
koakuma-chan <fresh.salt0811@fastmail.com>
2026-01-01 21:59:50 -0500

    Add #[rusqlite(try_from = T)]

--- README.md
+++ README.md
@@ -16,3 +16,26 @@ pub struct Username(
     String,
 );
 ```
+
+```rust
+use rusqlite_derive::FromSql;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, FromSql, ToSql)]
+#[serde(try_from = String)]
+#[rusqlite(try_from = String)]
+pub struct Username(String);
+
+impl TryFrom<String> for Username {
+    type Error = UsernameError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.len() >= 3 && value.len() <= 24 && value.chars().all(|c| c.is_alphanumeric()) {
+            Ok(Self(value))
+        } else {
+            Err(UsernameError::Invalid)
+        }
+    }
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,65 @@ use quote::quote;
 
 use syn::{parse_macro_input, Data, DeriveInput, Fields, Type};
 
-#[proc_macro_derive(FromSql)]
+struct RusqliteAttr {
+    try_from: Option<Type>,
+}
+
+fn parse_rusqlite_attr(input: &DeriveInput) -> syn::Result<RusqliteAttr> {
+    let mut attr = RusqliteAttr { try_from: None };
+
+    for attr_item in &input.attrs {
+        if attr_item.path().is_ident("rusqlite") {
+            attr_item.parse_nested_meta(|meta| {
+                if meta.path.is_ident("try_from") {
+                    let value: Type = meta.value()?.parse()?;
+
+                    attr.try_from = Some(value);
+
+                    Ok(())
+                } else {
+                    Err(meta.error("unknown rusqlite attribute"))
+                }
+            })?;
+        }
+    }
+
+    Ok(attr)
+}
+
+#[proc_macro_derive(FromSql, attributes(rusqlite))]
 pub fn from_sql(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+
+    let rusqlite_attr = match parse_rusqlite_attr(&input) {
+        Ok(attr) => attr,
+
+        Err(e) => {
+            return e
+                //
+                .to_compile_error()
+                //
+                .into();
+        }
+    };
+
+    let struct_name = &input.ident;
+
+    if let Some(try_from_type) = rusqlite_attr.try_from {
+        return quote! {
+            impl ::rusqlite::types::FromSql for #struct_name {
+                fn column_result(value: ::rusqlite::types::ValueRef<'_>) -> ::rusqlite::types::FromSqlResult<Self> {
+                    <#try_from_type as ::rusqlite::types::FromSql>::column_result(value)
+                        .and_then(|v| {
+                            <Self as ::core::convert::TryFrom<#try_from_type>>::try_from(v)
+                                .map_err(|e| ::rusqlite::types::FromSqlError::Other(::std::boxed::Box::new(e)))
+                        })
+                }
+            }
+        }
+        //
+        .into();
+    }
 
     let inner_type = match inner_type(&input) {
         Ok(inner_type) => inner_type,
@@ -19,8 +75,6 @@ pub fn from_sql(input: TokenStream) -> TokenStream {
                 .into();
         }
     };
-
-    let struct_name = &input.ident;
 
     quote! {
         impl ::rusqlite::types::FromSql for #struct_name {
